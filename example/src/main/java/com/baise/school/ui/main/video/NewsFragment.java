0072d667d7f61479550647fbcc14746f8eb17155
huagnzhiqiang <w739932799@qq.com>
2018-12-17 18:34:45 +0800

    添加讯飞语音朗读

--- example/src/main/java/com/baise/school/ui/main/video/NewsFragment.java
+++ example/src/main/java/com/baise/school/ui/main/video/NewsFragment.java
@@ -12,6 +12,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.baise.baselibs.app.AppConstants;
 import com.baise.baselibs.base.BaseFragment;
 import com.baise.baselibs.utils.GsonUtil;
 import com.baise.baselibs.utils.ToastUtils;
@@ -29,6 +30,7 @@ import com.gyf.barlibrary.ImmersionBar;
 import com.iflytek.cloud.RecognizerResult;
 import com.iflytek.cloud.SpeechConstant;
 import com.iflytek.cloud.SpeechError;
+import com.iflytek.cloud.SpeechSynthesizer;
 import com.iflytek.cloud.ui.RecognizerDialog;
 import com.iflytek.cloud.ui.RecognizerDialogListener;
 
@@ -281,6 +283,26 @@ public class NewsFragment extends BaseFragment<NewsPresenter> implements NewsCon
     };
 
 
+    public void onSynthesize(String newsText) {
+        //1.创建 SpeechSynthesizer 对象, 第二个参数： 本地合成时传 InitListener
+        SpeechSynthesizer mTts= SpeechSynthesizer.createSynthesizer(getContext(), null);
+        //2.合成参数设置，详见《 MSC Reference Manual》 SpeechSynthesizer 类
+        //设置发音人（更多在线发音人，用户可参见 附录13.2
+        mTts.setParameter(SpeechConstant.VOICE_NAME, "xiaoyan"); //设置发音人
+        mTts.setParameter(SpeechConstant.SPEED, "50");//设置语速
+        mTts.setParameter(SpeechConstant.VOLUME, "80");//设置音量，范围 0~100
+        mTts.setParameter(SpeechConstant.ENGINE_TYPE, SpeechConstant.TYPE_CLOUD); //设置云端
+        //设置合成音频保存位置（可自定义保存位置），保存在“./sdcard/iflytek.pcm”
+        //保存在 SD 卡需要在 AndroidManifest.xml 添加写 SD 卡权限
+        //仅支持保存为 pcm 和 wav 格式， 如果不需要保存合成音频，注释该行代码
+        mTts.setParameter(SpeechConstant.TTS_AUDIO_PATH, AppConstants.FILE_VOICE_CACHE + "iflytek.wav");
+
+        //3.开始合成
+        mTts.startSpeaking(newsText, null);
+    }
+
+
+
     /**
      * 请求数据
      *
@@ -358,7 +380,7 @@ public class NewsFragment extends BaseFragment<NewsPresenter> implements NewsCon
 
             //添加到数据库
             insertMsm(MsmAdapter.RECEIVER, data.getText(), getTime());
-
+            onSynthesize(data.getText());
         }
     }
 
